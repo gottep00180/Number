f568c18325abd6c5f8c018fba9fa38c0ef10cbd2
gottep00180 <58423093+gottep00180@users.noreply.github.com>
2021-08-18 14:00:46 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+number = int(input("กรอกตัวเลข: "))
+if number < 54:
+    print("น้อยไป")
+if number > 54:
+    print("มากเกินไป")
+if number == 54:
+    print("ถูกต้องค่ะ")
+while number != 54:
+    number = int(input("กรอกตัวเลข: "))
+    answer = 54
+    if number < 54:
+        print("น้อยไป")
+    if number > 54:
+        print("มากเกินไป")
+    if number == 54:
+        print("ถูกต้องค่ะ")
